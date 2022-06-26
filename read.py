c760640aea052ba2e4ca115426f62dbc6c0dafaa
Paschal27 <pco0004@auburn.edu>
2022-06-26 11:27:05 -0500

    1st change

--- read.py
+++ read.py
@@ -6,14 +6,14 @@ cv.imshow('Cat', img)
 
 cv.waitKey(0)
 
-# capture = cv.VideoCapture('Videos/dog.mp4')
+capture = cv.VideoCapture('Videos/dog.mp4')
 
-# while True:
-#     isTrue, frame = capture.read()
-#     cv.imshow('Video', frame)
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
 
-#     if cv.waitKey(20) & 0xFF==ord('d'):
-#         break
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
 
-# capture.release()
-# cv.destroyAllWindows()
+capture.release()
+cv.destroyAllWindows()
